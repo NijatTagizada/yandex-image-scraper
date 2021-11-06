d810e0b1c1ec79ef20533a4dfe918e304a789074
NijatTagizada <tagizada.nicat@gmail.com>
2021-11-06 17:11:04 +0400

    change some names, add some comments

--- scraper/_main_.py
+++ scraper/_main_.py
@@ -1,5 +1,5 @@
 from scraper.chrome_driver_setup import driver
-from scraper.export_failed_url import export_failed_links
+from scraper.export_failed_url import export_failed_urls
 from scraper.save_image import save_image, failed_url_list
 from scraper.scraping_data import scroll_down, get_data
 
@@ -7,13 +7,23 @@ url = 'https://yandex.ru/images/search?text=gopro&isize=large'
 
 if __name__ == '__main__':
     try:
+        # Open Chrome and navigate to Url
         driver.get(url)
+
+        # Scroll down for more image
         scroll_down(1)
+
+        # Get data from yandex
         url_list = get_data()
+
+        # Close driver
         driver.quit()
 
+        # Save image from exported url
         save_image(url_list)
-        export_failed_links(failed_url_list)
+
+        # Export failed url
+        export_failed_urls(failed_url_list)
     except Exception as ex:
         print(ex)
         driver.quit()
--- scraper/export_failed_url.py
+++ scraper/export_failed_url.py
@@ -1,11 +1,11 @@
 import csv
 
-csv_file = open('failed_links.csv', 'w')
+csv_file = open('failed_urls.csv', 'w')
 csv_writer = csv.writer(csv_file)
-csv_writer.writerow(['Links'])
+csv_writer.writerow(['Urls'])
 
 
-def export_failed_links(failed_url_list):
+def export_failed_urls(failed_url_list):
     for i in failed_url_list:
         csv_writer.writerow([i])
     csv_file.close()
